384a5a1c971542a9cb649e2257f723f1bf3e7496
GrenFORest <agurza23@yahoo.com>
2023-02-24 06:46:02 +0300

    new day

--- cw.js
+++ cw.js
@@ -2,9 +2,9 @@
 function countSheeps(arrayOfSheep) {
     let count = 0;
     for (let i = 0; i < arrayOfSheep.length; i++) {
-      if ( arrayOfSheep[i]) {
-        count++;
-      }
+        if (arrayOfSheep[i]) {
+            count++;
+        }
     }
     return count;
-  }
+}
--- fcc.js
+++ fcc.js
@@ -1,1 +1,21 @@
-alert(123);
+//23.02-Initializing Variables with the Assignment Operator
+/*
+let a = 9;
+*/
+//Declare String Variables
+/*
+var myFirstName = "Alex";
+var myLastName = "Har";
+*/
+// Only change code below this line
+var a;
+var b;
+var c;
+a = 5;
+b = 10;
+c = "I am a"
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
