9247329543e147b246cad42fd5f33ad6e5a8c9fc
GrenFORest <agurza23@yahoo.com>
2023-03-01 06:57:56 +0300

    +morning work 01.03

---
+++ fcc0103.js
@@ -0,0 +1,47 @@
+//01.03
+//Multiply Two Decimals with JavaScript
+const product = 2.0 * 2.5;
+
+//Divide One Decimal by Another with JavaScript
+const quotient = 4.4 / 2.0;
+
+//Finding a Remainder in JavaScript
+
+const remainder = 11 % 3;
+console.log(remainder)
+
+//Compound Assignment With Augmented Addition
+
+let a1 = 3;
+let b1 = 17;
+let c1 = 12;
+
+// Only change code below this line
+a1 += a1 + 9;
+b1 += -8 + b1;
+c1 += c1 + -5;
+
+console.log(a1, b1, c1)
+
+//Compound Assignment With Augmented Subtraction
+let a2 = 11;
+let b2 = 9;
+let c2 = 3;
+
+a2 -= a2 - 5;
+b2 -= b2 - -6;
+c2 -= c2 - 2;
+
+console.log(a2, b2, c2);
+
+//Compound Assignment With Augmented Multiplication
+
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+a *= 5;
+b *= 3;
+c *= 10;
+
+console.log(a, b, c);
