371a77d5404f28fd3c19459df2ae85d8b57d60f1
GrenFORest <agurza23@yahoo.com>
2023-03-11 19:57:53 +0300

    my work 1103

--- cw1103.js
+++ cw1103.js
@@ -1,9 +1,22 @@
 //11.03
+//Find Maximum and Minimum Values of a List
+
+let list = [4,6,2,1,9,63,-134,566];
 
 const min = function (list) {
-
+    let m = list[0];
+    for (i=1;i<list.length;i++) {
+        if (list[i] < m) m = list[i];
+    }
+    return m;
 }
 
 const max = function (list) {
-    
+    let m = list[0];
+    for (i=1;i<list.length;i++) {
+        if (list[i] > m) m = list[i];
+    }
+    return m;    
 }
+
+console.log(min(list), max(list));
--- fcc1103.js
+++ fcc1103.js
@@ -1,1 +1,8 @@
 //11.03
+//Appending Variables to Strings
+
+const someAdjective = "JavaScript!";
+let myStr = " Learning to code is ";
+myStr += someAdjective;
+
+console.log(myStr);
